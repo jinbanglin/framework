ae1f5007d7e8d1d5c1313f148cbab0c8a8435af2
jinbanglin <17golang@gmail.com>
2018-04-12 21:02:07 +0800

    update

--- auth/moss_jwt/jwt.go
+++ auth/moss_jwt/jwt.go
@@ -7,13 +7,8 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-const EXPIRES_DELTA = 3600 * 12
-
-//==============================jwt sha1==============================
+var MossJwtExpiresDelta int64 = 3600 * 12
 var JwtKey = []byte("8784e410796b279afea776524a6a464d7f9c153b")
-
-//==============================pwd sha1==============================
-var PasswordSalt = []byte("17160fcec5bd355fb8b770630d5a13401c9cd6cb")
 
 type Claims struct {
 	UserName string
@@ -28,7 +23,7 @@ func genJwtClaims(userName, userId, audience string) Claims {
 		UserId:   userId,
 		StandardClaims: jwt.StandardClaims{
 			Audience:  audience,
-			ExpiresAt: now.Unix() + EXPIRES_DELTA,
+			ExpiresAt: now.Unix() + MossJwtExpiresDelta,
 			Id:        now.Format("150405") + userId,
 			IssuedAt:  now.Unix(),
 			Issuer:    "bixin_user",
